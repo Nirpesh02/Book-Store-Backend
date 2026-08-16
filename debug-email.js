52b79407d2a384cd328d519d70c92557a618f227
Nirpesh02 <nirpeshdhungel7@gmail.com>
2026-08-16 12:39:25 +0545

    email

---
+++ debug-email.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import nodemailer from 'nodemailer';
+import fs from 'fs';
+
+async function testEmail() {
+  let log = '';
+  try {
+    const user = process.env.EMAIL_USER;
+    const pass = process.env.EMAIL_PASS;
+    log += `User: ${user}\nPass Length: ${pass ? pass.length : 0}\n`;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: { user, pass }
+    });
+
+    log += 'Verifying...\n';
+    await transporter.verify();
+    log += 'Verify Success!\n';
+    
+    fs.writeFileSync('email_debug.log', log);
+    console.log('Done');
+    process.exit(0);
+  } catch (err) {
+    log += `Error: ${err.message}\n`;
+    fs.writeFileSync('email_debug.log', log);
+    process.exit(1);
+  }
+}
+testEmail();
